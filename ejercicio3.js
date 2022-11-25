ea2f82f5bf2d24d510d828c23e0b6f22c8888c38
Melissa Ridente <melissaridente3e@gmail.com>
2022-11-25 22:18:05 +0100

    Iteración #3

---
+++ ejercicio3.js
@@ -0,0 +1,26 @@
+
+//Multiplica 10 por 5 y muestra el resultado mediante alert.
+var mult = 10 * 5; 
+console.log(mult);
+
+//Divide 10 por 2 y muestra el resultado en un alert.
+
+var div = 10 / 2;
+console.log(div);
+
+//Muestra mediante un alert el resto de dividir 15 por 9.
+var resto = 15 % 9;
+console.log(resto);
+
+//Usa el correcto operador de asignación que resultará en x = 15,  teniendo dos variables y = 10 y z = 5.
+
+
+// div = 5
+let y = 10;
+let x = div + y;
+console.log(x);
+
+//Usa el correcto operador de asignación que resultará en x = 50, teniendo dos variables y = 10 y z = 5.
+
+x = y * div;
+console.log(x);
